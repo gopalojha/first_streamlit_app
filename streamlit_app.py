7bccf76d3425c4829f303d605eb3fdbc3f9529cc
Gopal Ojha <gopal.ojha@gmail.com>
2023-01-26 17:01:57 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
-streamlit.title("My parents New Healthy Dinner")
+streamlit.title('My parents New Healthy Dinner')
+
+stramlit.header('Breakfast Menu')
+
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+
+streamlit.text('kale, Spinach & Rocket Smoothie')
+
+streamlit.text('Hard-Boiled Free-Range Egg')
